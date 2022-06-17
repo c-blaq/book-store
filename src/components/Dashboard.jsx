cefca14adc7f04b57fd2c664bb0297d86ef9a008
c-blaq <babayejuadesina@gmail.com>
2022-06-17 11:45:17 +0100

    dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { useAuth } from "../firebase";
 import bookCover from "../assets/images/book1.png";
+import bookCover2 from "../assets/images/book2.png";
+import { BsFillPersonFill, BsSearch, BsFillMicFill } from "react-icons/bs";
+
+import Swiper, { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const Dashboard = () => {
   const currentUser = useAuth();
+
+  const swiper = new Swiper(".swiper", {
+    spaceBetween: 10,
+    slidesPerView: 2,
+  });
+
   return (
     <div>
       <header className="my-4">
-        <span className="">{currentUser?.email}</span>
+        <div className="bg-gray-200 ml-auto rounded-[50%] w-10 h-10 flex items-center justify-center text-2xl text-gray-400 ">
+          <BsFillPersonFill />
+        </div>
       </header>
       <section>
         <div className="mb-8 sm:w-4/6">
@@ -15,61 +29,117 @@ const Dashboard = () => {
           <h2 className="text-2xl">What do you want to read today?</h2>
         </div>
 
-        <div>
+        <div className="relative">
+          <span className="absolute top-1/3 left-2 text-gray-400">
+            <BsSearch />
+          </span>
           <input
             type="text"
             placeholder="Search"
-            className="bg-gray-100 p-3 w-full rounded  shadow-sm"
+            className="bg-gray-100 p-3 pl-6 w-full rounded shadow-sm outline-none"
           />
+          <span className="top-1/3 right-2 absolute text-gray-400">
+            <BsFillMicFill />
+          </span>
         </div>
       </section>
 
       <section className="my-6">
-        <div className="flex gap-4 flex-nowrap mb-4 overflow-x-scroll">
-          <span className="block shrink-0 ">Novel</span>
-          <span className="block shrink-0 ">Self-love</span>
-          <span className="block shrink-0 ">sci-fi</span>
-          <span className="block shrink-0 ">romance</span>
-          <span className="block shrink-0 ">philosophy</span>
-          <span className="block shrink-0 ">philosophy</span>
+        <div className="swiper overflow-x-visible">
+          <div className="swiper-wrapper">
+            <span className=" swiper-slide ">Novel</span>
+            <span className=" swiper-slide ">Self-love</span>
+            <span className=" swiper-slide ">sci-fi</span>
+            <span className=" swiper-slide ">romance</span>
+            <span className=" swiper-slide ">philosophy</span>
+            <span className=" swiper-slide ">philosophy</span>
+          </div>
+          <div className="swipper-scrollbar"></div>
         </div>
 
-        <div className="flex gap-4 flex-nowrap mb-4 overflow-x-scroll">
-          <div className="shrink-0">
-            <img
-              className=" shadow-xl h-[200px] rounded-2xl"
-              src={bookCover}
-              alt="Book Cover"
-            />
-          </div>
-          <div className="shrink-0">
-            <img
-              className=" shadow-xl h-[200px] rounded-2xl"
-              src={bookCover}
-              alt="Book Cover"
-            />
-          </div>
-          <div className="shrink-0">
-            <img
-              className=" shadow-xl h-[200px] rounded-2xl"
-              src={bookCover}
-              alt="Book Cover"
-            />
-          </div>
-          <div className="shrink-0">
-            <img
-              className=" shadow-xl h-[200px] rounded-2xl"
-              src={bookCover}
-              alt="Book Cover"
-            />
-          </div>
+        <div className="swiper my-6">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide ">
+              <img
+                className=" shadow-xl h-[200px] rounded-2xl"
+                src={bookCover}
+                alt="Book Cover"
+              />
+            </div>
+            <div className="swiper-slide ">
+              <img
+                className=" shadow-xl h-[200px] rounded-2xl"
+                src={bookCover2}
+                alt="Book Cover"
+              />
+            </div>
+            <div className="swiper-slide ">
+              <img
+                className=" shadow-xl h-[200px] rounded-2xl"
+                src={bookCover}
+                alt="Book Cover"
+              />
+            </div>
+            <div className="swiper-slide ">
+              <img
+                className=" shadow-xl h-[200px] rounded-2xl"
+                src={bookCover2}
+                alt="Book Cover"
+              />
+            </div>
 
-          <div className="shrink-0">
-            <img
-              className=" shadow-xl h-[200px] rounded-2xl"
-              src={bookCover}
-              alt="Book Cover"
-            />
+            <div className="swiper-slide ">
+              <img
+                className=" shadow-xl h-[200px] rounded-2xl"
+                src={bookCover}
+                alt="Book Cover"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <h2 className="text-2xl font-bold">New Arrival</h2>
+
+        <div className="swiper my-6">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide ">
+              <img
+                className=" shadow-xl h-[200px] rounded-2xl"
+                src={bookCover}
+                alt="Book Cover"
+              />
+            </div>
+            <div className="swiper-slide ">
+              <img
+                className=" shadow-xl h-[200px] rounded-2xl"
+                src={bookCover2}
+                alt="Book Cover"
+              />
+            </div>
+            <div className="swiper-slide ">
+              <img
+                className=" shadow-xl h-[200px] rounded-2xl"
+                src={bookCover}
+                alt="Book Cover"
+              />
+            </div>
+            <div className="swiper-slide ">
+              <img
+                className=" shadow-xl h-[200px] rounded-2xl"
+                src={bookCover2}
+                alt="Book Cover"
+              />
+            </div>
+
+            <div className="swiper-slide ">
+              <img
+                className=" shadow-xl h-[200px] rounded-2xl"
+                src={bookCover}
+                alt="Book Cover"
+              />
+            </div>
           </div>
         </div>
       </section>
